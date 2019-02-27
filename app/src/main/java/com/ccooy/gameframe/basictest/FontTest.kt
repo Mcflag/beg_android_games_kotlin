71f29dbed7c27a1650dd37c074ebb833191a1fff
chen <chenchen@lcsw.cn>
2019-02-27 10:18:53 +0800

    fixbug

--- app/src/main/java/com/ccooy/gameframe/basictest/FontTest.kt
+++ app/src/main/java/com/ccooy/gameframe/basictest/FontTest.kt
@@ -1,17 +1,51 @@
 package com.ccooy.gameframe.basictest
 
+import android.content.Context
+import android.graphics.*
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
 
-class FontTest : SingleTouchTest() {
+class FontTest : AppCompatActivity() {
+
+    inner class RenderView(context: Context) : View(context) {
+        var paint: Paint
+        var font: Typeface
+        var bounds: Rect = Rect()
+
+        init {
+            paint = Paint()
+            font = Typeface.createFromAsset(context.assets, "font.ttf")
+        }
+
+        override fun onDraw(canvas: Canvas) {
+            canvas.drawRGB(0, 0, 0)
+            paint.color = Color.YELLOW
+            paint.typeface = font
+            paint.textSize = 28f
+            paint.textAlign = Paint.Align.CENTER
+            canvas.drawText("这是个测试This is a test!", canvas.width / 2f, 100f, paint)
+
+            val text = "这是另一个测试This is another test o_O"
+            paint.color = Color.WHITE
+            paint.textSize = 18f
+            paint.textAlign = Paint.Align.LEFT
+            paint.getTextBounds(text, 0, text.length, bounds)
+            canvas.drawText(text, canvas.width.toFloat() - bounds.width(), 140f, paint)
+            invalidate()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-        super.onCreate(savedInstanceState)
+        setContentView(RenderView(this))
     }
 
 }
--- app/src/main/java/com/ccooy/gameframe/basictest/MediaPlayerTest.kt
+++ app/src/main/java/com/ccooy/gameframe/basictest/MediaPlayerTest.kt
@@ -1,17 +1,49 @@
 package com.ccooy.gameframe.basictest
 
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import java.lang.Exception
 
-class MediaPlayerTest : SingleTouchTest() {
+class MediaPlayerTest : AppCompatActivity() {
+    var mediaPlayer: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
         super.onCreate(savedInstanceState)
+        var textView = TextView(this)
+        setContentView(textView)
+
+        volumeControlStream = AudioManager.STREAM_MUSIC
+        mediaPlayer = MediaPlayer()
+        try {
+            var descriptor = assets.openFd("music.ogg")
+            mediaPlayer?.let {
+                it.setDataSource(descriptor.fileDescriptor, descriptor.startOffset, descriptor.length)
+                it.prepare()
+                it.isLooping = true
+            }
+        } catch (e: Exception) {
+            textView.text = "Couldn't load music file, " + e.message
+            mediaPlayer = null
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mediaPlayer?.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mediaPlayer?.pause()
+        if (isFinishing) {
+            mediaPlayer?.let {
+                it.stop()
+                it.release()
+            }
+        }
     }
 
 }
